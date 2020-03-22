7c42d4f360c49c77554ca9856d27c885888b495a
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-22 16:51:55 -0300

    background component for all screens

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import PropTypes from 'prop-types';
 
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 export default function SignIn({ navigation }) {
   return (
-    <View>
-      <Text>SignIn</Text>
-      <Button
-        title="Go to SignUp"
-        onPress={() => navigation.navigate('SignUp')}
-      />
-    </View>
+    <Background>
+      <View>
+        <Text>SignIn</Text>
+        <Button
+          title="Go to SignUp"
+          onPress={() => navigation.navigate('SignUp')}
+        />
+      </View>
+    </Background>
   );
 }
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 export default function SignUp() {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <Background>
+      <View>
+        <Text>SignUp</Text>
+      </View>
+    </Background>
   );
 }
