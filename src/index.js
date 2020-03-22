d1eb0ce934c4569c47183ed9534f099879105f32
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-22 10:29:04 -0300

    remove unnecessary files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>Hello GoBarber</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
