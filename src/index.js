30ddaa51724378cac8c4cd2b583c410af084b505
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-23 15:09:22 -0300

    statubar setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import Routes from './routes';
 
 const App = () => {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
