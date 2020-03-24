45854c4daa5abaf42223bd7488e341766057e3bd
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-24 19:08:48 -0300

    styling app dashboard tab navigator

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-
 import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return <Background />;
 }
--- src/routes.js
+++ src/routes.js
@@ -3,22 +3,59 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useSelector } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const tabBarOptions = {
+  keyboardHidesTabBar: true,
+  activeTintColor: '#fff',
+  inactiveTintColor: 'rgba(255, 255, 255, 0.5)',
+  style: {
+    backgroundColor: '#8d41a8',
+    borderTopWidth: 0,
+  },
+};
+
 export default function Routes() {
   const signed = useSelector((state) => state.auth.signed);
+
+  const tabScreenOptions = ({ route }) => ({
+    // eslint-disable-next-line react/prop-types
+    tabBarIcon: ({ color, size }) => {
+      if (route.name === 'Dashboard') {
+        return <Icon name="event" size={size} color={color} />;
+      }
+      if (route.name === 'Profile') {
+        return <Icon name="person" size={size} color={color} />;
+      }
+      return <Icon name="person" size={size} color={color} />;
+    },
+  });
 
   return (
     <NavigationContainer>
       {signed ? (
-        <Tab.Navigator>
-          <Tab.Screen name="Dashboard" component={Dashboard} />
+        <Tab.Navigator
+          screenOptions={tabScreenOptions}
+          tabBarOptions={tabBarOptions}
+        >
+          <Tab.Screen
+            name="Dashboard"
+            component={Dashboard}
+            options={{ tabBarLabel: 'Agendamentos' }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{ tabBarLabel: 'Meu perfil' }}
+          />
         </Tab.Navigator>
       ) : (
         <Stack.Navigator
