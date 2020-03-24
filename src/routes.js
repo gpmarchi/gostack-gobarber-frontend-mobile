2e2f7cae78b6b7109a91bd8c6e2cbb994d029989
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-24 17:36:54 -0300

    initial routing based on signed in user

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,35 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useSelector } from 'react-redux';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Routes() {
+  const signed = useSelector((state) => state.auth.signed);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
+      {signed ? (
+        <Tab.Navigator>
+          <Tab.Screen name="Dashboard" component={Dashboard} />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
