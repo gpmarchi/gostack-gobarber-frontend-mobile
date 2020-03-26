0f8021130e7f68ab1b0127d5bccf9d8198d96cba
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-26 15:59:12 -0300

    new appointment select date page styling

---
+++ src/components/DateInput/index.android.js
@@ -0,0 +1,43 @@
+import React, { useMemo } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { DatePickerAndroid } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { Container, DateButton, DateText } from './styles';
+
+export default function DateInput({ date, onChange }) {
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
+    [date]
+  );
+
+  async function handleOpenPicker() {
+    const { action, year, month, day } = await DatePickerAndroid.open({
+      mode: 'spinner',
+      minDate: new Date(),
+      date,
+    });
+
+    if (action === DatePickerAndroid.dateSetAction) {
+      const selectedDate = new Date(year, month, day);
+
+      onChange(selectedDate);
+    }
+  }
+
+  return (
+    <Container>
+      <DateButton onPress={handleOpenPicker}>
+        <Icon name="event" color="#fff" size={20} />
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+    </Container>
+  );
+}
+
+DateInput.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
